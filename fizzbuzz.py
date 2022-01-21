36b5d9dc1985dbc73960b6c6823b3a5a0806b345
T <t@Ts-MacBook-Air.local>
2022-01-21 05:43:25 -0500

    adding fizbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def fizbuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print ('FizzBuzz')
+        elif i % 3 == 0:
+            print ('Fizz')
+        elif i % 5 == 0:
+            print ('Buzz')
+        else:
+            print (i)
+
+
+def main(argv):
+    n = int(argv[1])
+    fizbuzz(n)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
