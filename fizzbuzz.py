bdbabb5b796e15db81c691dbb35871841f54caa0
T <t@Ts-MacBook-Air.local>
2022-01-21 06:47:20 -0500

    restore fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,11 +4,11 @@ import sys
 def fizbuzz(n):
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print ('BizzFuzz')
+            print ('FizzBuzz')
         elif i % 3 == 0:
-            print ('Bizz') #Bizz
+            print ('Fizz')
         elif i % 5 == 0:
-            print ('Fuzz') #Fuzz
+            print ('Buzz')
         else:
             print (i)
 
