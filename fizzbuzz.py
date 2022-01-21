0969a09d0cf0f1d073b61db3bd9b0c139842cd06
T <t@Ts-MacBook-Air.local>
2022-01-21 06:26:51 -0500

    Changed Buzz to Fuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,11 +4,11 @@ import sys
 def fizbuzz(n):
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print ('FizzBuzz')
+            print ('FizzFuzz')
         elif i % 3 == 0:
             print ('Fizz')
         elif i % 5 == 0:
-            print ('Buzz')
+            print ('Fuzz') #Fuzz
         else:
             print (i)
 
